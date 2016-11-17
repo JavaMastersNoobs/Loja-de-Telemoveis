606034ac4689cad888927fe2b4f4981f333b5656
André Filipe <spitfire.andre@gmail.com>
2016-11-17 22:33:29 +0000

    sout

--- src/lojadetelemoveis/LojaDeTelemoveis.java
+++ src/lojadetelemoveis/LojaDeTelemoveis.java
@@ -17,6 +17,7 @@ public class LojaDeTelemoveis {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test2");
+        System.out.println("Final test");
     }
     
 }
