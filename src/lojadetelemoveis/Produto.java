9906a0d5dc7b80060a1e1b8395b73bcdde9c00f1
André Filipe <spitfire.andre@gmail.com>
2016-11-18 23:35:53 +0000

    classe produto

--- src/lojadetelemoveis/Produto.java
+++ src/lojadetelemoveis/Produto.java
@@ -10,5 +10,31 @@ package lojadetelemoveis;
  * @author andra
  */
 public class Produto {
-    
+    String marca;
+    String modelo;
+    int ano;
+    String processador;
+    int rom;
+    int ram;
+    int traseira;
+    int frontal;
+    int ecra; 
+    String so;
+    double preco;
+
+    public Produto(String marca, String modelo)
+            {
+            marca="";
+            modelo="";
+            ano=0;
+            processador="";
+            rom=0;
+            ram=0;
+            traseira=0;
+            frontal=0;
+            ecra=0;
+            so="";
+            preco=0;                 
+            }
 }
+    
