c5af680c243ebd9121a597889155de6de15c0158
André Filipe <spitfire.andre@gmail.com>
2016-11-19 18:51:40 +0000

    novo telemovel?

--- src/lojadetelemoveis/LojaDeTelemoveis.java
+++ src/lojadetelemoveis/LojaDeTelemoveis.java
@@ -18,6 +18,18 @@ public class LojaDeTelemoveis {
         System.out.println("test");
         System.out.println("test2");
         System.out.println("Final test");
+        Produto Tech = new Produto();
+        Tech.setmarca("wt");
+        Tech.setmodelo("arroz");
+        Tech.setano(2014);
+        Tech.setprocessador("SD 801");
+        Tech.setrom(16);
+        Tech.setram(2);
+        Tech.settraseira(13);
+        Tech.setfrontal(5);
+        Tech.setso("Android 4.4 KitKat");
+        Tech.setpreco(199);
+        
     }
     
 }
