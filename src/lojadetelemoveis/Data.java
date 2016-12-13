ccb6870cfacc0a82b366f2f2b5a302405467a7c4
André Filipe <spitfire.andre@gmail.com>
2016-12-13 01:17:08 +0000

    Update Data.java

--- src/lojadetelemoveis/Data.java
+++ src/lojadetelemoveis/Data.java
@@ -1,29 +1,29 @@
 package lojadetelemoveis;
+
 import java.io.Serializable;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import static lojadetelemoveis.Ler.umaString;
+
 public class Data implements Serializable {
+
     private Date date;
-    
-    public Data()
-    {
+
+    public Data() {
         date = new Date();
     }
-    public Data(Date d)
-    {
+
+    public Data(Date d) {
         date = d;
     }
-    public String getData () 
-    {
+
+    public String getData() {
         return java.text.DateFormat.getDateInstance(DateFormat.SHORT).format(date);
     }
-    
-    public int nVendas (String d) 
-    {
-        int n = 0;
-        return (n);
+
+    public Date formatDate() {
+        return date;
     }
-    
+
 }
