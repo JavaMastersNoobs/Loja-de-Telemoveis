b9d8f810514b39e00cd005328b194b3e825a1040
André Filipe <spitfire.andre@gmail.com>
2016-12-13 01:18:18 +0000

    Update Ler.java

--- src/lojadetelemoveis/Ler.java
+++ src/lojadetelemoveis/Ler.java
@@ -1,93 +1,79 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lojadetelemoveis;
 
-/**
- *
- * @author Gabriel
- */
 import java.io.*;
+
 public class Ler {
-public static String umaString(){
-    String s = "";
-    try{
-        BufferedReader in = new BufferedReader ( new InputStreamReader (System.in));
-        s = in.readLine();
-        
-    }
-    catch(IOException e){
-        System.out.println("Erro ao ler");
-    }
-    return s;
-}
 
-public static int umInt(){
-    while(true){
-        try{
-            return Integer.valueOf(umaString().trim());
-        }
-        catch(Exception e){
-            System.out.print("Inválido, introduza novamente:");
-        }
-    }
-}
+    public static String umaString() {
+        String s = "";
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            s = in.readLine();
 
-public static float umFloat(){
-    while(true){
-        try{
-            return Float.valueOf(umaString().trim());
+        } catch (IOException e) {
+            System.out.println("Erro ao ler");
         }
-        catch(Exception e){
-            System.out.print("Inválido, introduza novamente:");
-        }
+        return s;
     }
-}
 
-public static double umDouble(){
-    while(true){
-        try{
-            return Double.valueOf(umaString().trim());
-        }
-        catch(Exception e){
-            System.out.print("Inválido, introduza novamente:");
+    public static int umInt() {
+        while (true) {
+            try {
+                return Integer.valueOf(umaString().trim());
+            } catch (Exception e) {
+                System.out.print("Inválido, introduza novamente:");
+            }
         }
     }
-}
 
-public static long umLong(){
-    while(true){
-        try{
-            return Long.valueOf(umaString().trim());
-        }
-        catch(Exception e){
-            System.out.print("Inválido, introduza novamente:");
+    public static float umFloat() {
+        while (true) {
+            try {
+                return Float.valueOf(umaString().trim());
+            } catch (Exception e) {
+                System.out.print("Inválido, introduza novamente:");
+            }
         }
     }
-}
 
-public static boolean umBoolean(){
-    while(true){
-        try{
-            return Boolean.valueOf(umaString().trim());
-        }
-        catch(Exception e){
-            System.out.print("Inválido, introduza novamente:");
+    public static double umDouble() {
+        while (true) {
+            try {
+                return Double.valueOf(umaString().trim());
+            } catch (Exception e) {
+                System.out.print("Inválido, introduza novamente:");
+            }
         }
     }
-}
 
-public static char umChar(){
-    while(true){
-        try{
-            return umaString().trim().charAt(0);
-        }
-        catch(Exception e){
-            System.out.print("Inválido, introduza novamente:");
+    public static long umLong() {
+        while (true) {
+            try {
+                return Long.valueOf(umaString().trim());
+            } catch (Exception e) {
+                System.out.print("Inválido, introduza novamente:");
+            }
         }
     }
-}
+
+    public static boolean umBoolean() {
+        while (true) {
+            try {
+                return Boolean.valueOf(umaString().trim());
+            } catch (Exception e) {
+                System.out.print("Inválido, introduza novamente:");
+            }
+        }
+    }
+
+    public static char umChar() {
+        while (true) {
+            try {
+                return umaString().trim().charAt(0);
+            } catch (Exception e) {
+                System.out.print("Inválido, introduza novamente:");
+            }
+        }
+    }
 
 }
